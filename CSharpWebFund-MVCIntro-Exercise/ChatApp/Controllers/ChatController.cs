814e3dd5d3df39bdbb5b5841fab23a78aedb49d4
aEnchev669 <aenchev669@abv.bg>
2024-01-30 21:46:15 +0300

    Create ChatController.cs

---
+++ CSharpWebFund-MVCIntro-Exercise/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,43 @@
+﻿using ChatApp.Models.Chat;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace ChatApp.Controllers
+{
+	public class ChatController : Controller
+	{
+
+		private static readonly List<KeyValuePair<string, string>> messages = new List<KeyValuePair<string, string>>();
+
+		public IActionResult Show()
+		{
+			if (messages.Count < 0)
+			{
+				return this.View(new ChatViewModel());
+			}
+
+			var chatViewModel = new ChatViewModel()
+			{
+				AllMessages = messages
+				.Select(m => new MessageViewModel()
+				{
+					Sender = m.Key,
+					MessageText = m.Value
+				})
+				.ToArray()
+			};
+
+			return this.View(chatViewModel);
+		}
+
+		[HttpPost]
+		public IActionResult Send(ChatViewModel chat)
+		{
+			var newMessage = chat.CurrentMessage;
+
+			messages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.MessageText));
+
+			return RedirectToAction("Show");
+		}
+	}
+}
