4338848f63af20c52853da5b212e53f95605f828
aEnchev669 <aenchev669@abv.bg>
2024-02-28 21:01:10 +0300

    Create ErrorViewModel.cs

---
+++ AspNetCoreAdvanceDemo/AspNetCoreAdvanceDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreAdvanceDemo.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
