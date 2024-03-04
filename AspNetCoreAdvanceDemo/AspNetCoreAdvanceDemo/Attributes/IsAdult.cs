bfb693ce9683c9c37ceb319483abf550c5a8a972
aEnchev669 <aenchev669@abv.bg>
2024-03-04 22:09:45 +0300

    Create IsAdult.cs

---
+++ AspNetCoreAdvanceDemo/AspNetCoreAdvanceDemo/Attributes/IsAdult.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreAdvanceDemo.Attributes
+{
+	public class IsAdult : ValidationAttribute
+	{
+		private readonly DateTime minimumAge = DateTime.Today.AddYears(-18);
+
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (value != null && (DateTime)value <= minimumAge)
+			{
+				return ValidationResult.Success;
+			}
+
+			return new ValidationResult(ErrorMessage);
+		}
+	}
+}
