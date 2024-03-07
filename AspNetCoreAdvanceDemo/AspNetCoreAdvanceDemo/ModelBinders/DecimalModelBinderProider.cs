cd04f780d7d62ee0cbde487e5a7bcd207856457f
aEnchev669 <aenchev669@abv.bg>
2024-03-07 20:31:03 +0300

    Create DecimalModelBinderProider.cs

---
+++ AspNetCoreAdvanceDemo/AspNetCoreAdvanceDemo/ModelBinders/DecimalModelBinderProider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AspNetCoreAdvanceDemo.ModelBinders
+{
+	public class DecimalModelBinderProider : IModelBinderProvider
+	{
+		public IModelBinder? GetBinder(ModelBinderProviderContext context)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException("context");
+			}
+
+			if (context.Metadata.ModelType == typeof(decimal)
+				|| context.Metadata.ModelType == typeof(decimal?))
+			{
+				return new DecimalModelBinder();
+			}
+
+			return null;
+		}
+	}
+}
