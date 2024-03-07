3992949392cb321a845848e801b45082f0447545
aEnchev669 <aenchev669@abv.bg>
2024-03-07 20:31:04 +0300

    Create HomeViewModel.cs

---
+++ AspNetCoreAdvanceDemo/AspNetCoreAdvanceDemo/Models/HomeViewModel.cs
@@ -0,0 +1,32 @@
+﻿using AspNetCoreAdvanceDemo.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreAdvanceDemo.Models
+{
+	public class HomeViewModel : IValidatableObject
+	{
+
+		[IsAdult(ErrorMessage = "Must be at least 18 years old")]
+        public DateTime MyDate { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (string.IsNullOrEmpty(Name))
+			{
+				yield return new ValidationResult("Name is required");
+			}
+
+			if (string.IsNullOrEmpty(Country))
+			{
+				yield return new ValidationResult("Country is required");
+			}
+
+			if (Name == "Pesho" && Country != "BG")
+			{
+				yield return new ValidationResult("If name is Pesho, Country must be BG");
+			}
+		}
+	}
+}
